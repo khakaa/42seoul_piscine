2d99dec9f6d6ccc574e1222b366b756b55e350a4
박하린 <suyeopbuin@baghalin-ui-MacBookAir.local>
2021-02-27 17:27:19 +0900

    0227

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,66 +1,23 @@
 #include <unistd.h>
 
-void	print_num(char fir[], char sec[])
+void ft_print_comb(void)
 {
-	write(1, &fir[0], 1);
-	write(1, &fir[1], 1);
-	write(1, " ", 1);
-	write(1, &sec[0], 1);
-	write(1, &sec[1], 1);
-	return ;
-}
-
-void	second_num(char fir[], char sec[])
-{
-	sec[0] = '0';
-	while (sec[0] <= '9')
+	char num[5];
+	num[0] = '0' - 1;
+	num[3] = ',';
+	num[4] = ' ';
+	
+	while(++num[0] <= '6')
 	{
-		sec[1] = '0';
-		while (sec[1] <= '9')
+		num[1] = num[0];
+		while(++num[1] <= '8')
 		{
-			if (fir[0] <= sec[0])
+			num[2] = num[1];
+			while(++num[2] <= '9')
 			{
-				if (fir[0] == sec[0] && fir[1] >= sec[1])
-				{
-					sec[1]++;
-					continue ;
-				}
-				print_num(fir, sec);
-				if (!(fir[0] == '9' && fir[1] == '8'))
-					write(1, ", ", 2);
+				write(1, num, 5);
 			}
-			sec[1]++;
 		}
-		sec[0]++;
 	}
-	return ;
+	write(1, "789", 3);
 }
-
-void	first_num(char fir[], char sec[])
-{
-	while (fir[0] <= '9')
-	{
-		fir[1] = '0';
-		while (fir[1] <= '9')
-		{
-			second_num(fir, sec);
-			fir[1]++;
-		}
-		fir[0]++;
-	}
-	return ;
-}
-
-void	ft_print_comb(void)
-{
-	char	fir[2];
-	char	sec[2];
-
-	fir[0] = '0';
-	fir[1] = '0';
-	sec[0] = '0';
-	sec[1] = '1';
-	first_num(fir, sec);
-	return ;
-}
-
