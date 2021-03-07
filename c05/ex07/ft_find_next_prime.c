4894ee47d374e32dc89694e8cff22283a8c638aa
박하린 <phr0210@naver.com>
2021-03-07 16:09:59 +0900

    0307

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -1,4 +1,26 @@
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	i = 2;
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	ft_find_next_prime(int nb)
 {
+	int i;
 
+	i = nb;
+	while (!(ft_is_prime(i)))
+		i++;
+	return (i);
 }
---
+++ c05/ex07/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int	ft_is_prime(int nb);
+
+int	ft_find_next_prime(int nb);
+
+int main()
+{
+	printf("%d", ft_find_next_prime(8));
+}
